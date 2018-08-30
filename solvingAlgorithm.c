5501ad6952b644f8b7fdae041e58c4e742920f1e
Konkord360 <Konkord720@gmail.com>
2018-08-31 00:16:49 +0200

    step back to original version

--- solvingAlgorithm.c
+++ solvingAlgorithm.c
@@ -159,8 +159,3 @@ int countPossibleWays(WayOut *current, const int width) {
 		current->node = current->node + 1;
 	return possibleWays;
 }
-
-
-void dodajmyFunkcjêTestowaZebySprawdzicCzyDobrzeDzialaGit() {
-	return 0;
-}
